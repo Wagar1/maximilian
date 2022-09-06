a0d199ad83776a891bb3ed700080f11c05bd57d1
Wagar1 <vuqar.eyvazov.1997@gmail.com>
2022-09-06 14:45:26 +0400

    testing with vitest

---
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -0,0 +1,18 @@
+import { expect, it } from "vitest";
+import { transformToNumber } from "./numbers";
+
+it('should transform a string number to a number of type number', () => {
+    const value = '1';
+
+    const result = transformToNumber(value);
+
+    expect(result).toBeTypeOf('number');
+});
+
+it('should yield NaN for non-transformable values', () => {
+    const value = 'v';
+
+   const result = transformToNumber(value);
+    
+    expect(result).toBeNaN();
+});
---
+++ basic-testing-frontend/src/util/validation.test.js
@@ -0,0 +1,40 @@
+import { expect, it } from "vitest";
+import { validateNumber, validateStringNotEmpty } from "./validation";
+
+it('to throw an error if an empty string value passed to the function', () => {
+    const input = '';
+
+    const resultFn = () => { validateStringNotEmpty(input); }
+
+    expect(resultFn).toThrow('');
+});
+
+it('should not throw an error if non-empty string will be provided', () => {
+    const input = 'value';
+
+    const resultFn = () => { validateStringNotEmpty(input); };
+
+    expect(resultFn).not.toThrow();
+});
+
+it('should throw an error if anything except string will be provided', () => {
+    const inputInt = 1;
+    const inputBoolean = true;
+
+    const resultFn = () => { validateStringNotEmpty(inputInt); }
+    const resultFn2 = () => { validateStringNotEmpty(inputBoolean); }
+
+    expect(resultFn).toThrow();
+    expect(resultFn2).toThrow();
+});
+
+it('should throw an error if non-numeric value was provided', () => {
+    const input = NaN;
+    const input2 = '1';
+
+    const resultFn = () => { validateNumber(input); }
+    const resultFn2 = () => { validateNumber(input2); }
+
+    expect(resultFn).toThrow();
+    expect(resultFn2).toThrow();
+});
