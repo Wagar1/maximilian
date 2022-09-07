3a78d0e5981541c05624f9699bde084d4ea5056f
Wagar1 <vuqar.eyvazov.1997@gmail.com>
2022-09-07 10:38:57 +0400

    mocks and spies end

--- 01-starting-project/src/util/io.test.js
+++ 01-starting-project/src/util/io.test.js
@@ -3,11 +3,20 @@ import { promises as fs } from 'fs';
 import writeData from './io';
 
 vi.mock('fs');
+vi.mock('path', () => {
+    return {
+        default: {
+            join: (...args) => {
+                return args[args.length - 1]
+            }
+        }
+    };
+});
 
-it('should execute the writeFile method', () => {
+it('should return a promise that resolves to no value if called correctly', () => {
     const testData = "Test";
     const testFileName = "test.txt";
-    writeData(testData, testFileName);
-    //return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
-    expect(fs.writeFile).toBeCalled();
+    //writeData(testData, testFileName);
+    return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+    //expect(fs.writeFile).toBeCalledWith(testFileName, testData);
 });
